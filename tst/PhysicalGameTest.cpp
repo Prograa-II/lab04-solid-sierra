b1f88c3dc94676649fd9172f122c23ddc00dd4cb
Felipe <felipemb15t@outlook.com>
2020-08-25 14:42:31 -0600

    MinGW actualizado

--- tst/PhysicalGameTest.cpp
+++ tst/PhysicalGameTest.cpp
@@ -11,10 +11,12 @@ TEST(PhysicalGameTestSuite, CalculateDiscount){
     physicalGame.setItemWeight(0.564);
     physicalGame.setProductDimensions("3.94 x 0.59 x 5.91 inches; 0.56 Ounces");
     physicalGame.setPrice(24.66);
+    EXPECT_DOUBLE_EQ(physicalGame.getPrice(), 24.66);
     physicalGame.setTax(0.13);
     physicalGame.setDiscount(0.13);
     physicalGame.calculatePriceWithTax();
     physicalGame.calculatePriceWithDiscount();
 
     EXPECT_DOUBLE_EQ(physicalGame.getPrice(), 24.243245999999999);
+
 }
